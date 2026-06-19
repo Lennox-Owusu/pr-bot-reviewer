7c4ae440b3071744be77ff2673ab299298167396
Lennox-Owusu <lennymep82@gmail.com>
2026-06-19 13:09:47 +0000

    qa p2: re-trigger after config fix

--- payment_service.py
+++ payment_service.py
@@ -2,3 +2,5 @@ def get_user(db, user_id):
     # BUG: SQL injection via string formatting
     query = "SELECT * FROM users WHERE id = '%s'" % user_id
     return db.execute(query).fetchone()
+
+# re-trigger review
