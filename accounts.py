e9be1a6d5e3bb4d126f89440ea003367c06583f4
Lennox-Owusu <lennox.afriyie@amalitechtraining.org>
2026-06-29 12:59:20 +0000

    fix: parameterize query to remove SQL injection (AI-1189 round 2)

--- accounts.py
+++ accounts.py
@@ -2,6 +2,6 @@ import sqlite3
 
 
 def find_account(db, name):
-    query = "SELECT * FROM accounts WHERE name = '" + name + "'"
-    cur = db.execute(query)
+    query = "SELECT * FROM accounts WHERE name = ?"
+    cur = db.execute(query, (name,))
     return cur.fetchall()
